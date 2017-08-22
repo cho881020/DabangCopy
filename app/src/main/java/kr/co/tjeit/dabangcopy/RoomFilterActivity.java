92999482e0741a4ee8f78e5a32e3b5142db2c16d
cho881020 <cho881020@gmail.com>
2017-08-22 18:19:33 +0900

    방 필터 연습문제 마무리

--- app/src/main/java/kr/co/tjeit/dabangcopy/RoomFilterActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/RoomFilterActivity.java
@@ -1,9 +1,16 @@
 package kr.co.tjeit.dabangcopy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class RoomFilterActivity extends BaseActivity {
+
+    private android.widget.Button okBtn;
+    private android.widget.ToggleButton monthPayToggleBtn;
+    private android.widget.ToggleButton charterToggleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,17 @@ public class RoomFilterActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("월세선택여부", monthPayToggleBtn.isChecked());
+                intent.putExtra("전세선택여부", charterToggleBtn.isChecked());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
     }
 
     @Override
@@ -26,6 +44,9 @@ public class RoomFilterActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.charterToggleBtn = (ToggleButton) findViewById(R.id.charterToggleBtn);
+        this.monthPayToggleBtn = (ToggleButton) findViewById(R.id.monthPayToggleBtn);
+        this.okBtn = (Button) findViewById(R.id.okBtn);
 
     }
 }
--- app/src/main/java/kr/co/tjeit/dabangcopy/RoomListActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/RoomListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,10 +16,16 @@ import kr.co.tjeit.dabangcopy.util.GlobalData;
 
 public class RoomListActivity extends BaseActivity {
 
+    private final int REQ_FOR_FILTER = 1;
+
+    boolean isMonthPaySelected = true;
+    boolean isCharterSelected = true;
+
     private android.widget.ListView roomListView;
 //    필터되서 출력될 수 있도록 지원해주는 출력용 리스트
     List<Room> mDisplayRoomArray = new ArrayList<>();
     RoomAdapter mAdapter;
+    private android.widget.ImageView filterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,15 @@ public class RoomListActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, RoomFilterActivity.class);
+                startActivityForResult(intent, REQ_FOR_FILTER);
+            }
+        });
+
         roomListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -49,6 +65,71 @@ public class RoomListActivity extends BaseActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_FOR_FILTER) {
+            if (resultCode == RESULT_OK) {
+//                실제로 데이터 필터를 적용하는 일.
+//                1. 전세/월세 선택 여부를 저장
+
+                isMonthPaySelected = data.getBooleanExtra("월세선택여부", true);
+                isCharterSelected = data.getBooleanExtra("전세선택여부", true);
+
+//                2. 선택여부를 가지고 실제로 필터
+
+                filterAndRefreshListView();
+
+            }
+        }
+
+    }
+
+    private void filterAndRefreshListView() {
+
+//        1. 출력용 리스트를 전부 비워줌.
+
+        mDisplayRoomArray.clear();
+
+//        2. 비워진 출력용 리스트에 조건에 맞는 방들을 추가
+
+//        2.1 조건에 맞는지 모든 방 (GlobalData => allRooms) 들을 검사
+
+        for (Room room : GlobalData.allRooms) {
+//            전/월세가 상황이 맞는지 저장하는 변수
+//            기본적으로는 조건에 맞지 않는다고 전제
+            boolean isPayOk = false;
+
+//            월세를 포함하는지?
+            if (isMonthPaySelected) {
+//                월세가 0보다 큰지? => 맞으면 월세다.
+                if (room.getRentPay() > 0) {
+                    isPayOk = true;
+                }
+
+            }
+
+            if (isCharterSelected) {
+                if (room.getRentPay() == 0) {
+                    isPayOk = true;
+                }
+            }
+
+
+//            상황이 맞다면 실제로 추가하는 부분.
+            if (isPayOk) {
+                mDisplayRoomArray.add(room);
+            }
+
+
+        }
+
+//        리스트뷰의 새로고침
+        mAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
     public void setValues() {
         mAdapter = new RoomAdapter(mContext, mDisplayRoomArray);
         roomListView.setAdapter(mAdapter);
@@ -57,6 +138,6 @@ public class RoomListActivity extends BaseActivity {
     @Override
     public void bindViews() {
         this.roomListView = (ListView) findViewById(R.id.roomListView);
-
+        this.filterBtn = (ImageView) findViewById(R.id.filterBtn);
     }
 }
