fdb8f53593ca6021209999ac151a6fea22a23046
cho881020 <cho881020@gmail.com>
2017-08-23 11:10:06 +0900

    방 목록 필터 (지불방식 / 방갯수) 마무리

--- app/src/main/java/kr/co/tjeit/dabangcopy/RoomFilterActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/RoomFilterActivity.java
@@ -8,14 +8,47 @@ import android.widget.ToggleButton;
 
 public class RoomFilterActivity extends BaseActivity {
 
+    private Button okBtn;
+    private ToggleButton monthPayToggleBtn;
+    private ToggleButton charterToggleBtn;
+    private ToggleButton oneRoomToggleBtn;
+    private ToggleButton twoRoomToggleBtn;
+    private ToggleButton threeRoomToggleBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_filter);
+        bindViews();
+        setupEvents();
+        setValues();
     }
 
     @Override
     public void setupEvents() {
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                이 intent는 화면을 이동하기 위한 intent가 아님
+//                단순히 데이터를 첨부하는 용도로 쓰기 위한 intent
+//                그래서 객체화 할때 생성자에 파라미터를 비워둠.
+                Intent finishIntent = new Intent();
+//                돌려보낼 데이터를 finishIntent에 첨부
+                finishIntent.putExtra("월세선택여부", monthPayToggleBtn.isChecked());
+                finishIntent.putExtra("전세선택여부", charterToggleBtn.isChecked());
+                finishIntent.putExtra("원룸선택여부", oneRoomToggleBtn.isChecked());
+                finishIntent.putExtra("투룸선택여부", twoRoomToggleBtn.isChecked());
+                finishIntent.putExtra("쓰리룸선택여부", threeRoomToggleBtn.isChecked());
+
+//                실제로 돌려보내는 작업 -> setResult, finish
+//                setResult : 확인 표시, 어떤 데이터를 첨부하는지 전달
+                setResult(RESULT_OK, finishIntent);
+//                화면을 종료
+                finish();
+
+            }
+        });
 
     }
 
@@ -27,5 +60,11 @@ public class RoomFilterActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
+        this.threeRoomToggleBtn = (ToggleButton) findViewById(R.id.threeRoomToggleBtn);
+        this.twoRoomToggleBtn = (ToggleButton) findViewById(R.id.twoRoomToggleBtn);
+        this.oneRoomToggleBtn = (ToggleButton) findViewById(R.id.oneRoomToggleBtn);
+        this.charterToggleBtn = (ToggleButton) findViewById(R.id.charterToggleBtn);
+        this.monthPayToggleBtn = (ToggleButton) findViewById(R.id.monthPayToggleBtn);
+        this.okBtn = (Button) findViewById(R.id.okBtn);
     }
 }
--- app/src/main/java/kr/co/tjeit/dabangcopy/RoomListActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/RoomListActivity.java
@@ -18,14 +18,21 @@ public class RoomListActivity extends BaseActivity {
 
     private final int REQ_FOR_FILTER = 1;
 
+//    월세 선택 여부를 저장하는 변수 => 기본값 : true
     boolean isMonthPaySelected = true;
+//    전세 선택 여부를 저장하는 변수
     boolean isCharterSelected = true;
+//    원룸, 투룸, 쓰리룸 선택여부를 저장할 변수
+    boolean isOneRoomeSelected = true;
+    boolean isTwoRoomeSelected = true;
+    boolean isThreeRoomeSelected = true;
 
     private android.widget.ListView roomListView;
 //    필터되서 출력될 수 있도록 지원해주는 출력용 리스트
     List<Room> mDisplayRoomArray = new ArrayList<>();
     RoomAdapter mAdapter;
     private android.widget.ImageView filterBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,14 @@ public class RoomListActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, RoomFilterActivity.class);
+                startActivityForResult(myIntent, REQ_FOR_FILTER);
+            }
+        });
+
         roomListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -54,6 +69,107 @@ public class RoomListActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_FOR_FILTER) {
+            if (resultCode == RESULT_OK) {
+//                 필터를 가서 && 확인을 누른
+
+//                intent data가 첨부한 데이터를 들고있으니
+//                반드시 data.get~~Extra하도록 하자.
+//                getIntent() 와 별개 임을 기억하자.
+
+                isMonthPaySelected = data.getBooleanExtra("월세선택여부", true);
+                isCharterSelected = data.getBooleanExtra("전세선택여부", true);
+                isOneRoomeSelected = data.getBooleanExtra("원룸선택여부", true);
+                isTwoRoomeSelected = data.getBooleanExtra("투룸선택여부", true);
+                isThreeRoomeSelected = data.getBooleanExtra("쓰리룸선택여부", true);
+
+//                실제로 출력용 리스트에 데이터를 필터하는 메쏘드
+                filterRoomList();
+
+            }
+        }
+
+
+    }
+
+    private void filterRoomList() {
+//        이 메쏘드가 실제로 출력용 리스트인 mDisplayRoomArray를 가공하는 역할
+
+//        필터 : 표시될 목록을 전부 삭제부터.
+        mDisplayRoomArray.clear();
+
+//        월세/전세 계약 방식이 조건에 맞는지를 저장하는 변수
+        boolean isPayOk = false;
+//        방의 갯수가 조건에 맞는지를 저장하는 변수.
+        boolean isRoomCountOk = false;
+
+//        전체 데이터를 하나하나 검사.
+        for (Room room : GlobalData.allRooms) {
+
+//            1번 질문. 월세를 보여줄건지?
+            if (isMonthPaySelected) {
+//                1.1번 질문. 지금 검사하는 방이 실제로 월세인지?
+                if (room.getRentPay() > 0) {
+//                    월세를 보여줄 상황이기도 하고, 실제로 월세이기도 하니
+//                    이 방은 보여줘야 한다.
+
+                    isPayOk = true;
+
+                }
+            }
+
+            if (isCharterSelected) {
+                if (room.getRentPay() == 0) {
+                    isPayOk = true;
+                }
+            }
+
+            if (isOneRoomeSelected) {
+                if (room.getRoomCount() == 1) {
+                    isRoomCountOk = true;
+                }
+            }
+
+            if (isTwoRoomeSelected) {
+                if (room.getRoomCount() == 2) {
+                    isRoomCountOk = true;
+                }
+            }
+
+            if (isThreeRoomeSelected) {
+                if (room.getRoomCount() == 3) {
+                    isRoomCountOk = true;
+                }
+            }
+
+
+
+//            마지막 질문. 모든 조건을 만족시키는 방인지?
+            if (isPayOk && isRoomCountOk) {
+//                모든 조건이 다 맞는 방이다.
+//                실제로 출력해주자. -> 출력용 리스트에 집어넣자.
+
+                mDisplayRoomArray.add(room);
+
+            }
+
+//            검사가 끝났으니, 다시 기본값을 설정
+//            계약방식도, 방 갯수도 일단 안맞는다라고 전제.
+            isPayOk = false;
+            isRoomCountOk = false;
+
+
+        }
+
+//        모든 검사가 끝나면, 필터된 데이터를 가지고 리스트뷰 새로고침
+        mAdapter.notifyDataSetChanged();
+
+
     }
 
     @Override
