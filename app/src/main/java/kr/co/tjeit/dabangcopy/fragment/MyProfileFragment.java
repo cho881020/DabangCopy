d214989b03b2f17b54ebf9814347b601bb5c971b
cho881020 <cho881020@gmail.com>
2017-08-25 13:20:06 +0900

    마이프로필 프래그먼트 작업 마무리

--- app/src/main/java/kr/co/tjeit/dabangcopy/fragment/MyProfileFragment.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/fragment/MyProfileFragment.java
@@ -1,11 +1,14 @@
 package kr.co.tjeit.dabangcopy.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import kr.co.tjeit.dabangcopy.R;
 
@@ -15,15 +18,29 @@ import kr.co.tjeit.dabangcopy.R;
 
 public class MyProfileFragment extends Fragment {
 
+    private android.widget.TextView dialCustomerCenter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_my_profile, container, false);
+        this.dialCustomerCenter = (TextView) v.findViewById(R.id.dialCustomerCenter);
         return v;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setupEvents();
+    }
+
+    private void setupEvents() {
+        dialCustomerCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:07042113951"));
+                startActivity(intent);
+            }
+        });
     }
 }
