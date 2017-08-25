0840901b4ae6316a6e6ff8d4838359d147b52fcc
cho881020 <cho881020@gmail.com>
2017-08-25 12:05:44 +0900

    메인화면 탭바 마무리

--- app/src/main/java/kr/co/tjeit/dabangcopy/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/MainActivity.java
@@ -13,6 +13,12 @@ public class MainActivity extends BaseActivity {
     private android.widget.LinearLayout myProfileFragmentLayout;
     private LinearLayout homeBtn;
     private LinearLayout myBtn;
+    private LinearLayout favoriteListFragmentLayout;
+    private LinearLayout mapListFragmentLayout;
+    private LinearLayout seeMoreFragmentLayout;
+    private LinearLayout favoriteBtn;
+    private LinearLayout mapBtn;
+    private LinearLayout seeMoreBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +36,38 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
-        homeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                homeFragmentLayout.setVisibility(View.VISIBLE);
-                myProfileFragmentLayout.setVisibility(View.GONE);
-            }
-        });
+//        1. OnClickListener 변수화
+//        2. Tag 이용
 
-        myBtn.setOnClickListener(new View.OnClickListener() {
+        final LinearLayout[] frags = {homeFragmentLayout, favoriteListFragmentLayout,
+        mapListFragmentLayout, myProfileFragmentLayout, seeMoreFragmentLayout};
+
+        View.OnClickListener tabListner = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                homeFragmentLayout.setVisibility(View.GONE);
-                myProfileFragmentLayout.setVisibility(View.VISIBLE);
+//                눌리면 할 일
+
+//                모든 Fragment 를 다 숨겨주는 작업.
+                for (LinearLayout linearLayout : frags) {
+                    linearLayout.setVisibility(View.GONE);
+                }
+
+//                필요한 프래그먼트를 나타내는 작업.
+//                1) 눌린 버튼에 달려있는 태그를 숫자(int)로 변환
+                int index = Integer.parseInt(v.getTag().toString());
+//                2) 변환된 int변수 index를 활용해 원하는 프래그먼트(를 담은 LinearLayout)를
+//                   다시 표시하도록 설정.
+                frags[index].setVisibility(View.VISIBLE);
+
             }
-        });
+        };
+
+        homeBtn.setOnClickListener(tabListner);
+        favoriteBtn.setOnClickListener(tabListner);
+        mapBtn.setOnClickListener(tabListner);
+        myBtn.setOnClickListener(tabListner);
+        seeMoreBtn.setOnClickListener(tabListner);
+
 
     }
 
@@ -55,10 +78,15 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.seeMoreBtn = (LinearLayout) findViewById(R.id.seeMoreBtn);
         this.myBtn = (LinearLayout) findViewById(R.id.myBtn);
+        this.mapBtn = (LinearLayout) findViewById(R.id.mapBtn);
+        this.favoriteBtn = (LinearLayout) findViewById(R.id.favoriteBtn);
         this.homeBtn = (LinearLayout) findViewById(R.id.homeBtn);
+        this.seeMoreFragmentLayout = (LinearLayout) findViewById(R.id.seeMoreFragmentLayout);
         this.myProfileFragmentLayout = (LinearLayout) findViewById(R.id.myProfileFragmentLayout);
+        this.mapListFragmentLayout = (LinearLayout) findViewById(R.id.mapListFragmentLayout);
+        this.favoriteListFragmentLayout = (LinearLayout) findViewById(R.id.favoriteListFragmentLayout);
         this.homeFragmentLayout = (LinearLayout) findViewById(R.id.homeFragmentLayout);
     }
 }
