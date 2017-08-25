729df399134db03c69b06fa8c04f15f6635ca247
cho881020 <cho881020@gmail.com>
2017-08-25 10:51:44 +0900

    메인화면 작업준비

--- app/src/main/java/kr/co/tjeit/dabangcopy/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/MainActivity.java
@@ -1,11 +1,15 @@
 package kr.co.tjeit.dabangcopy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import kr.co.tjeit.dabangcopy.util.GlobalData;
 
 public class MainActivity extends BaseActivity {
+
+
+    private android.widget.LinearLayout homeFragmentLayout;
+    private android.widget.LinearLayout myProfileFragmentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,11 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 //        임시로, 이곳에서 GlobalData에 더미데이터를 채워넣음.
         GlobalData.initGlobalData();
+
+        bindViews();
+        setupEvents();
+        setValues();
+
     }
 
     @Override
@@ -28,5 +37,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
+        this.myProfileFragmentLayout = (LinearLayout) findViewById(R.id.myProfileFragmentLayout);
+        this.homeFragmentLayout = (LinearLayout) findViewById(R.id.homeFragmentLayout);
     }
 }
