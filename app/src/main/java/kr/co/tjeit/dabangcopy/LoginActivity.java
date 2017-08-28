5ec0cf884501bd3100b8e08382d0c3ed0717be30
cho881020 <cho881020@gmail.com>
2017-08-28 09:46:39 +0900

    로그인 액티비티 연습문제

--- app/src/main/java/kr/co/tjeit/dabangcopy/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/dabangcopy/LoginActivity.java
@@ -1,9 +1,17 @@
 package kr.co.tjeit.dabangcopy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends BaseActivity {
+
+    private android.widget.EditText idEdt;
+    private android.widget.Button loginBtn;
+    private com.facebook.login.widget.LoginButton loginbutton;
+    private android.widget.Button facebookLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,8 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+//        1. 아이디를 입력한 상태에서 로그인 버튼을 누르면 메인액티비티로 이동
+//        2. 프로필 설정화면에서 나타나는 이메일을, 이곳에서 입력하는 ID가 표시되도록.
 
     }
 
@@ -26,6 +36,11 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+
+        this.facebookLoginBtn = (Button) findViewById(R.id.facebookLoginBtn);
+        this.loginbutton = (LoginButton) findViewById(R.id.login_button);
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
 
     }
 }
